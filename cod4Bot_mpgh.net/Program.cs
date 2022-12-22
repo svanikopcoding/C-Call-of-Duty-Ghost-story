062793062d4e32f0000f86e860957408d9d992bb
Svanik OP Coding <73551008+svanikopcoding@users.noreply.github.com>
2022-12-22 21:19:26 +0530

    Ghost Story

---
+++ cod4Bot_mpgh.net/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Whats your name?");
+        Console.ReadKey();
+        Console.ReadLine();
+        Console.WriteLine("Ok so whats yor age?");
+        Console.ReadKey();
+        Console.ReadLine();
+        Console.WriteLine("So Lets play a story.");
+        Console.ReadKey();
+        Console.WriteLine("Once upon a time there was a man name Ghost and he lived in ?");
+        Console.ReadKey();
+        Console.ReadLine();
+        Console.WriteLine("And one day he become a soldier and was in a group name Task Forse 141 and was seen in which game?");
+        Console.ReadKey();
+        Console.ReadLine();
+    
+    }
+}
